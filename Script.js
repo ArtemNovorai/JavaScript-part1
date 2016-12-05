8d50ca33dccd2155af94e58bdfaf1c77a17108e1
Artem <artnovorai@gmail.com>
2016-12-05 17:37:36 +0200

    JS1

---
+++ Script.js
@@ -0,0 +1,5 @@
+var name = prompt("Please enter your name", "Artem");
+var time = prompt("The meeting began?", "12:00am");
+var meeting = prompt("What will happen?", "the meeting");
+var place = prompt("In what city meeting?", "Lviv");
+console.log(name + " rides on " +  meeting + " at " + time + " in " + place);
